8884c61963141ed1246a2b9da320bf090166146a
Flaminiat <47352051+Flaminiat@users.noreply.github.com>
2019-11-12 12:28:32 +0100

    files of the client side added

---
+++ build/precache-manifest.14869faab93258d7cf31bd7e0a81dbd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a032cad951d45a242b58a8cede8fde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a6b5b7a65e75ff5f91",
+    "url": "/static/css/main.444d1d88.chunk.css"
+  },
+  {
+    "revision": "5ff760ff5ea2f9847410",
+    "url": "/static/js/2.fe618751.chunk.js"
+  },
+  {
+    "revision": "e6a6b5b7a65e75ff5f91",
+    "url": "/static/js/main.a7ee22d9.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
